243fdbd39e28633f5d31fa630060ffd81fcf261d
csarsound <csarsound@hotmail.com>
2022-07-06 16:36:01 -0500

    FirstUser

--- console.py
+++ console.py
@@ -8,6 +8,7 @@ from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models import storage
 import shlex
+from models.amenity import Amenity
 
 
 class HBNBCommand(cmd.Cmd):
