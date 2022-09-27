5710f88bf2540ddc2bb83b77a3d8a0c7b5e88edd
csarsound <csarsound@hotmail.com>
2022-09-27 13:58:42 -0500

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,77 +1,44 @@
 #!/usr/bin/python3
-"""
-Module for BaseModel class
+"""This is the Base Model module.
+Contains the BaseModel class which will be the
+"base" of all other classes in this project.
 """
 import uuid
 from datetime import datetime
 import models
-import json
-format_iso = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel():
     """
-    BaseModel class
-    Defines all common attributes/methods for other classes:
-    Instance attributes:
-        Public:
-            - id
-            - created_at
-            - updated_at
-    Instance methods:
-        Public:
-            - save(self)
-            - to_dict(self)
-    Special method:
-        - __init__(self)
-        - __str__(self)
+    The goal is to manage all common attributes and methods for other classes.
+    Attributes:
+        id (str): unique random ID for each BaseModel instance.
+        created_at (datetime): the current datetime when instance is created.
+        updated_at (datetime): the current datetime when instance is updated.
     """
 
     def __init__(self, *args, **kwargs):
-
+        """Initializes the default attributes of the BaseModel object.
+        Args:
+            *args: unused.
+            **kwargs (dict): a dictionary containing wanted attributes.
         """
-    Initializes an instance of BaseModel class
-        """
-
-        if kwargs is None or len(kwargs) == 0:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
-        else:
-            for key, value in kwargs.items():
-                if key == "id":
-                    self.id = value
-                elif key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, format_iso)
-                elif key is "__class__":
-                    pass
-                elif key is not "__class__":
-                    self.__dict__[key] = value
+        self.id = str(uuid.uuid4())
+        self.create_at = self.updated_at = datetime.now()
 
     def __str__(self):
-        """
-    Returns a string representation of an instance
-        """
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+        """overrides the default behaviour of the __str__ method."""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """
-    Updates the public instance update_at with the current time
-        """
+        """Updates the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
-        """
-        Returns a dictionary containing all keys/values
-        of __dict__ of the instance
-        """
-        my_dic = dict(self.__dict__)
-        # make variable my_dict
-        my_dic["__class__"] = self.__class__.__name__
-        # add class key with class name of the object
-        my_dic["created_at"] = self.created_at.isoformat()
-        my_dic["updated_at"] = self.updated_at.isoformat()
-        return (my_dic)
+        """Returns a dictionary with all the keys/value of the instance."""
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = self.create_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
