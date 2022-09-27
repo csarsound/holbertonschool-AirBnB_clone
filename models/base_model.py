0573c2e93eb3e5ef52911da0db0e1634b85432a0
csarsound <csarsound@hotmail.com>
2022-09-27 15:48:49 -0500

    BaseModelFromDictionary

--- models/base_model.py
+++ models/base_model.py
@@ -25,7 +25,7 @@ class BaseModel():
         """
         self.id = str(uuid.uuid4())
         self.create_at = self.updated_at = datetime.now()
-        if kwargs is not None an kwargs != {}:
+        if kwargs is not None and kwargs != {}:
             for k, v in kwargs.items():
                 if k in ["created_at", "updates_at"]:
                     v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
@@ -38,6 +38,10 @@ class BaseModel():
     def save(self):
         """Updates the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
+
+    def created_at(self):
+        """assign the current datetime"""
+        self.create_at = datetime.now()
 
     def to_dict(self):
         """Returns a dictionary with all the keys/value of the instance."""
